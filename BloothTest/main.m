e3543b2e637a0dc7454d9ee901c7ee68be2714c5
ting <ting@wujietingdeMacBook-Pro.local>
2014-12-01 16:09:27 +0800

    Initial Commit

---
+++ BloothTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BloothTest
+//
+//  Created by ting on 14-12-1.
+//  Copyright (c) 2014年 yuanbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
